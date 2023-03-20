e31e3235b526e37fef777cdb2b37c9df2b89e0f1
Emmanuel manucho <manuchoadero@gmail.com>
2023-03-20 19:53:04 +0300

    battries tests

---
+++ test/test_battries.py
@@ -0,0 +1,61 @@
+import unittest
+from datetime import datetime
+
+from batteries.nubbin_battery import NubbinBattery
+from batteries.spindler_battery import SpindlerBattery
+
+
+class TestNubbin(unittest.TestCase):
+    def setUp(self) -> None:
+        self.current_date: datetime = datetime.today().date()
+        self.last_service_date: datetime = datetime.today().date()
+        self.battery: NubbinBattery = None
+
+    def create_nubbin_battery(self):
+        self.battery = NubbinBattery(self.current_date, self.last_service_date)
+
+    def test_battery_should_be_serviced(self):
+        self.last_service_date = self.current_date.replace(
+            year=self.current_date.year - 5
+        )
+
+        self.create_nubbin_battery()
+        self.assertTrue(self.battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        self.last_service_date = self.current_date.replace(
+            year=self.current_date.year - 3
+        )
+
+        self.create_nubbin_battery()
+        self.assertFalse(self.battery.needs_service())
+
+
+class TestSpindler(unittest.TestCase):
+    def setUp(self) -> None:
+        self.current_date: datetime = datetime.today().date()
+        self.last_service_date: datetime = datetime.today().date()
+        self.battery: SpindlerBattery = None
+
+    def create_spindler_battery(self):
+        self.battery = SpindlerBattery(self.current_date, self.last_service_date)
+
+    def test_battery_should_be_serviced(self):
+        self.last_service_date = self.current_date.replace(
+            year=self.current_date.year - 3
+        )
+
+        self.create_spindler_battery()
+        self.assertTrue(self.battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        self.last_service_date = self.current_date.replace(
+            year=self.current_date.year - 2
+        )
+
+        self.create_spindler_battery()
+        self.assertFalse(self.battery.needs_service())
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/test_engines.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class TestCapulet(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        pass
+    
+    def test_engine_should_not_be_serviced(self):
+        pass
+
+
+class TestSternman(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        pass
+    
+    def test_engine_should_not_be_serviced(self):
+        pass
+
+
+class TestWilloughby(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        pass
+    
+    def test_engine_should_not_be_serviced(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
